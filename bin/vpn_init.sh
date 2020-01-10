af66594d5e7efa9cff8f701a8ff5c1a4e8946417
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2020-01-10 20:22:25 +0100

    move settings to the root folder

--- bin/vpn_init.sh
+++ bin/vpn_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $(dirname $(dirname $(realpath $0)))
-source ./bin/settings.sh
+source .env
 
 docker-compose run --rm openvpn ovpn_genconfig -u udp://${SERVER_ADDRESS}
 docker-compose run --rm openvpn ovpn_initpki
